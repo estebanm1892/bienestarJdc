f6f4be56aca481e284e3327dad573548a0e99116
Esteban Monroy B <estebanm1892@gmail.com>
2020-04-16 11:42:23 -0500

    calls

--- Android Bienestar/app/src/main/java/com/esteban/bienestarjdc/repository/AreaRepository.kt
+++ Android Bienestar/app/src/main/java/com/esteban/bienestarjdc/repository/AreaRepository.kt
@@ -3,10 +3,11 @@ package com.esteban.bienestarjdc.repository
 import com.esteban.bienestarjdc.data.Area
 import com.esteban.bienestarjdc.network.MyApi
 import io.reactivex.Observable
+import retrofit2.Call
 
 
 class AreaRepository(private val apiService: MyApi) {
-    fun getAreas(): Observable<List<Area>> {
+    fun getAreas(): Call<List<Area>> {
         return apiService.getAreas
     }
 }
--- Android Bienestar/app/src/main/java/com/esteban/bienestarjdc/repository/NormativeRepository.kt
+++ Android Bienestar/app/src/main/java/com/esteban/bienestarjdc/repository/NormativeRepository.kt
@@ -3,9 +3,10 @@ package com.esteban.bienestarjdc.repository
 import com.esteban.bienestarjdc.data.Normative
 import com.esteban.bienestarjdc.network.MyApi
 import io.reactivex.Observable
+import retrofit2.Call
 
 class NormativeRepository(private val apiService: MyApi) {
-    fun getNormatives(): Observable<List<Normative>> {
+    fun getNormatives(): Call<List<Normative>> {
         return apiService.getNormatives
     }
 }
